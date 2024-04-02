98577b2a16b8f62df349ea695ab60aecf8887b5f
Matějíček <matejkonsel3@seznam.cz>
2024-04-02 11:21:39 +0200

    first commit

---
+++ src/pages/About/About.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom"
+
+
+export default function About () {
+         return (
+            <>
+            <h1>About</h1>
+            <Link to={"/"}>
+            <p>Home</p>    
+            </Link>
+            </>
+         )
+}
---
+++ src/pages/Clicker/Clicker.jsx
@@ -0,0 +1,36 @@
+import { Link } from "react-router-dom"
+import { useState, useEffect } from "react"
+import "./Clicker.css"
+
+export default function Clicker() {
+   
+    // [nazev_promenne, nazev setteru] = useState(vychozi_hodnota);
+    const [cookies, setCookies] = useState(0);
+
+    //setter - funkce ktera prenastavuje promennou
+
+    const increaseCookies = () => {
+        setCookies(cookies + 1);
+    }
+
+    useEffect(() => {
+        document.title = "Cookie clicker loaded";
+    }, []); //kdyz [] prazdne - funkce se spusti kdyz se stranka nacte
+
+
+    useEffect(() => {
+        document.title = cookies;
+    }, [cookies]); //kdyz se ta hodnota v hranatych zavorkach zmeni tak se zavola ta funkce
+
+
+    return(
+        <>
+            <h1>Clicker</h1>
+            <button onClick={increaseCookies} className="clicker-button">Click me</button>
+            <p>Cookies: {cookies}</p>
+            <Link to={"/"}>
+                <p>Go home</p>
+            </Link>
+        </>
+    )
+}
